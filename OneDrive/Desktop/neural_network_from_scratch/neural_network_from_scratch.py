b5d3ca7b005d46d63abc154f8b849a2ec2770d76
osamh09 <osamaForEdu@gmail.com>
2025-11-10 14:38:57 +0300

    Initail commit

---
+++ OneDrive/Desktop/neural_network_from_scratch/neural_network_from_scratch.py
@@ -0,0 +1,7 @@
+# A single random neural node in a fully connected neural network that has a three connected nodes inputs and baise 
+inputs = [1, 2, 3]
+weights = [2.1, 3.2, 5.5]
+baise = 3
+
+output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + baise
+print(output)
